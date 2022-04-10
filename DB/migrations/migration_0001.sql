b5caca96d6a293d0698d956b35d5c870abf36580
cggonzal <gonzalezcgg1@gmail.com>
2022-04-10 11:51:13 -0700

    changed migration to create table if it does not exist

--- DB/migrations/migration_0001.sql
+++ DB/migrations/migration_0001.sql
@@ -1,5 +1,5 @@
 -- NOTE: "SERIAL" is the postgres version of auto incrementing a value
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL
