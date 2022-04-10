602206b572107d6d426a5bc86a9e8f8505113bd7
cggonzal <gonzalezcgg1@gmail.com>
2022-04-10 13:46:17 -0700

    added comment to migration

--- DB/migrations/migration_0001.sql
+++ DB/migrations/migration_0001.sql
@@ -1,4 +1,4 @@
--- NOTE: "SERIAL" is the postgres version of auto incrementing a value
+-- NOTE: "SERIAL" is the postgres version of auto incrementing a value. Has type integer with max value (2^31) - 1
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   email TEXT UNIQUE NOT NULL,
