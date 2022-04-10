40525776f672bca0c41f31c11ddd0632a101b936
cggonzal <gonzalezcgg1@gmail.com>
2022-04-10 13:50:17 -0700

    removed comment

--- local_env_vars.sh
+++ local_env_vars.sh
@@ -2,5 +2,5 @@ export PORT=8000
 export RDS_USERNAME=cgg  # use psql to create locally if does not exist
 export RDS_PASSWORD=mypass  # use psql to create locally if does not exist
 export RDS_DB_NAME=mytestdb  # use psql to create locally if does not exist
-export RDS_HOSTNAME=localhost  # use psql to create locally if does not exist
+export RDS_HOSTNAME=localhost
 export RDS_PORT=5432  # default postgres port
