958ecb15aecbe5538e5707b867f845dbc2432167
cggonzal <gonzalezcgg1@gmail.com>
2022-04-16 22:59:29 -0700

    added secret key to local tests

--- local_env_vars.sh
+++ local_env_vars.sh
@@ -4,3 +4,4 @@ export RDS_PASSWORD=mypass  # use psql to create locally if does not exist
 export RDS_DB_NAME=mytestdb  # use psql to create locally if does not exist
 export RDS_HOSTNAME=localhost
 export RDS_PORT=5432  # default postgres port
+export SECRET_KEY=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa  # only used for testing
