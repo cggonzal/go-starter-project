38eb7dcb16b2cd55e9bf5ab3355072dce3e5fa95
cggonzal <gonzalezcgg1@gmail.com>
2022-06-22 19:44:46 -0700

    update to migrations to remove if_not_exists

--- DB/migrations/migration_0001.sql
+++ DB/migrations/migration_0001.sql
@@ -1,5 +1,5 @@
 -- NOTE: "SERIAL" is the postgres version of auto incrementing a value. Has type integer with max value (2^31) - 1
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   email TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL
