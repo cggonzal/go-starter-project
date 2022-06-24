5ffdf7cccc8d033820b951117c23a1ceb56a45a0
cggonzal <gonzalezcgg1@gmail.com>
2022-06-23 20:04:48 -0700

    updated env vars to be named as postgres expects

--- DB/DB.go
+++ DB/DB.go
@@ -12,14 +12,14 @@ var (
 
 func InitDB() {
 	// Connect to the postgres db
-	RDS_USERNAME := os.Getenv("RDS_USERNAME")
-	RDS_PASSWORD := os.Getenv("RDS_PASSWORD")
-	RDS_DB_NAME := os.Getenv("RDS_DB_NAME")
-	RDS_HOSTNAME := os.Getenv("RDS_HOSTNAME")
-	RDS_PORT := os.Getenv("RDS_PORT")
+	PGUSER := os.Getenv("PGUSER")
+	PGPASSWORD := os.Getenv("PGPASSWORD")
+	PGDATABASE := os.Getenv("PGDATABASE")
+	PGHOST := os.Getenv("PGHOST")
+	PGPORT := os.Getenv("PGPORT")
 
 	connStr := fmt.Sprintf("user=%s dbname=%s sslmode=disable host=%s port=%s password=%s",
-		RDS_USERNAME, RDS_DB_NAME, RDS_HOSTNAME, RDS_PORT, RDS_PASSWORD)
+		PGUSER, PGDATABASE, PGHOST, PGPORT, PGPASSWORD)
 
 	var err error
 	DBCon, err = sql.Open("postgres", connStr)
--- local_env_vars.sh
+++ local_env_vars.sh
@@ -1,7 +1,7 @@
 export PORT=8000
-export RDS_USERNAME=cgg  # use psql to create locally if does not exist
-export RDS_PASSWORD=mypass  # use psql to create locally if does not exist
-export RDS_DB_NAME=mytestdb  # use psql to create locally if does not exist
-export RDS_HOSTNAME=localhost
-export RDS_PORT=5432  # default postgres port
+export PGUSER=cgg  # use psql to create locally if does not exist
+export PGPASSWORD=mypass  # use psql to create locally if does not exist
+export PGDATABASE=mytestdb  # use psql to create locally if does not exist
+export PGHOST=localhost
+export PGPORT=5432  # default postgres port
 export SECRET_KEY=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa  # only used for testing
