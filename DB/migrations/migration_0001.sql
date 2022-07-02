585fc0f451a0ea4895697a35b7263da38273b30c
cggonzal <gonzalezcgg1@gmail.com>
2022-07-01 19:12:55 -0700

    changed id type to bigserial

--- DB/migrations/migration_0001.sql
+++ DB/migrations/migration_0001.sql
@@ -1,6 +1,6 @@
 -- NOTE: "SERIAL" is the postgres version of auto incrementing a value. Has type integer with max value (2^31) - 1
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
+  id BIGSERIAL PRIMARY KEY,
   email TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL
 );
