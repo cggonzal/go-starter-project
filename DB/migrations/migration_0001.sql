a56aaab83a74cdb9393e29280fff5994c417aca0
cggonzal <gonzalezcgg1@gmail.com>
2022-07-03 14:29:51 -0700

    updated comment

--- DB/migrations/migration_0001.sql
+++ DB/migrations/migration_0001.sql
@@ -1,4 +1,4 @@
--- NOTE: "SERIAL" is the postgres version of auto incrementing a value. Has type integer with max value (2^31) - 1
+-- NOTE: "SERIAL" is the postgres version of auto incrementing a value. Has type integer with max value (2^31) - 1. BIGSERIAL is 64 bit.
 CREATE TABLE users (
   id BIGSERIAL PRIMARY KEY,
   email TEXT UNIQUE NOT NULL,
