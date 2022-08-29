1988e7d671c9f7afcbcb4c06be81ef4a91470ff2
cggonzal <gonzalezcgg1@gmail.com>
2022-08-28 21:10:24 -0700

    added upload endpoint

--- main.go
+++ main.go
@@ -19,6 +19,37 @@ func about(w http.ResponseWriter, _ *http.Request) {
 	templates.AboutTemplate.Execute(w, data)
 }
 
+func upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		// TODO: change GET method to create a template that returns an html page showing all uploaded files with hyper links...
+		http.ServeFile(w, r, "./static/upload.html")
+		return
+	}
+
+	logger := customLogger.GetLogger()
+
+	// get uploaded file
+	file, header, err := r.FormFile("filename")
+	if err != nil {
+		logger.Fatal("error getting file...", err)
+	}
+
+	data := make([]byte, header.Size)
+	_, err = file.Read(data)
+	if err != nil {
+		logger.Fatal("error reading file: ", err)
+	}
+
+	err = os.WriteFile("./savedFiles/"+header.Filename, data, 0666)
+	if err != nil {
+		logger.Fatal("Error saving file:", err)
+	}
+	logger.Printf("saved file to savedFiles/%s", header.Filename)
+
+	// redirect to landing page
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
 func main() {
 	// serve landing page
 	http.HandleFunc("/", index)
@@ -33,6 +64,7 @@ func main() {
 	http.HandleFunc("/logout", customUser.Logout)
 	http.HandleFunc("/delete", customUser.Delete)
 	http.HandleFunc("/secret", customUser.Secret)
+	http.HandleFunc("/upload", upload)
 
 	// initialize Logger, this has to come before all other initializations since they use the logger
 	customLogger.InitLogger()
